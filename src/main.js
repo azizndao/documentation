a28da7620435ae962f39ab05dc43ab360376f18d
Abdou Aziz Ndao <azizndao71@gmail.com>
2020-09-28 15:49:30 +0000

    Adding firebase package

--- src/main.js
+++ src/main.js
@@ -3,13 +3,27 @@ import App from './App.vue'
 import './utils/registerServiceWorker'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/analytics'
+import 'firebase/performance'
+import 'firebase/firestore'
+import firebaseConfig from '@/utils/firebaseConfig'
 
 Vue.config.productionTip = false
 
+let app = firebase.initializeApp(firebaseConfig)
+app.performance()
+app.analytics()
+
+Vue.prototype.$firebase = app
+Vue.prototype.$auth = app.auth()
+Vue.prototype.$firestore = app.firestore()
+
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDVNMPe1lo7ljDdFZuBCFm4WNuqtAz2FO0",
+    authDomain: "z-docs.firebaseapp.com",
+    databaseURL: "https://z-docs.firebaseio.com",
+    projectId: "z-docs",
+    storageBucket: "z-docs.appspot.com",
+    messagingSenderId: "512701406735",
+    appId: "1:512701406735:web:08f1f8534808953e89cd7a",
+    measurementId: "G-6F6KC5PZV1"
+};
